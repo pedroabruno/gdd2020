3a13b3e31c27feddeec6e426b0cf74c751aa49b3
Nahuel Pascual <npascual@est.frba.utn.edu.ar>
2020-10-03 11:29:05 -0300

    agrego nombre a las FK

--- Concesionaria/clearTables.sql
+++ Concesionaria/clearTables.sql
@@ -1,3 +1,6 @@
+drop table gd_esquema.Item;
+drop table gd_esquema.Operacion;
+drop table gd_esquema.Tipo_Operacion;
 drop table gd_esquema.Auto;
 drop table gd_esquema.Tipo_Transmision;
 drop table gd_esquema.Tipo_Caja;
@@ -5,9 +8,5 @@ drop table gd_esquema.Tipo_Auto;
 drop table gd_esquema.Auto_Parte;
 drop table gd_esquema.Modelo;
 drop table gd_esquema.Fabricante;
-drop table gd_esquema.Factura;
 drop table gd_esquema.Cliente;
 drop table gd_esquema.Sucursal;
-drop table gd_esquema.Item;
-drop table gd_esquema.Operacion;
-drop table gd_esquema.Tipo_Operacion;

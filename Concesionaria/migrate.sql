03ff902bd4815a5dc600722d5b53ce74e9bcb352
Nahuel Pascual <npascual@est.frba.utn.edu.ar>
2020-10-24 17:41:49 -0300

    migracion Auto

--- Concesionaria/migrate.sql
+++ Concesionaria/migrate.sql
@@ -10,6 +10,38 @@ from gd_esquema.Maestra
 where SUCURSAL_MAIL is not null;
 
 -- Se cargan datos de la tabla 'tipo_auto'
-insert into gd.esquema.Tipo_Auto(TIPO_AUTO_CODIGO,TIPO_AUTO_DESC)
-select distinct (tipo_auto_codigo), tipo_auto_desc
-from gd_esquema.Maestra;
+insert into gd_esquema.Tipo_Auto(TIPO_AUTO_CODIGO,TIPO_AUTO_DESC)
+select distinct tipo_auto_codigo, tipo_auto_desc
+from gd_esquema.Maestra
+where tipo_auto_codigo is not null;
+
+-- Se cargan datos de la tabla 'tipo_transmision'
+insert into gd_esquema.Tipo_Transmision(TIPO_TRANSMISION_CODIGO,TIPO_TRANSMISION_DESC)
+select distinct TIPO_TRANSMISION_CODIGO, TIPO_TRANSMISION_DESC
+from gd_esquema.Maestra
+where TIPO_TRANSMISION_CODIGO is not null;
+
+-- Se cargan datos de la tabla 'tipo_caja'
+insert into gd_esquema.Tipo_Caja(TIPO_CAJA_CODIGO,TIPO_CAJA_DESC)
+select distinct TIPO_CAJA_CODIGO, TIPO_CAJA_DESC
+from gd_esquema.Maestra
+where TIPO_CAJA_CODIGO is not null;
+
+-- Se cargan datos de la tabla 'fabricante'
+insert into gd_esquema.Fabricante(FABRICANTE_NOMBRE)
+select distinct FABRICANTE_NOMBRE
+from gd_esquema.Maestra
+where FABRICANTE_NOMBRE is not null;
+
+-- Se cargan datos de la tabla 'modelo'
+insert into gd_esquema.Modelo(MODELO_CODIGO,MODELO_NOMBRE,MODELO_FABRICANTE,MODELO_TIPO_TRANSMISION,MODELO_TIPO_CAJA,MODELO_TIPO_MOTOR,MODELO_TIPO_AUTO)
+select distinct MODELO_CODIGO,MODELO_NOMBRE,f.fabricante_id,TIPO_TRANSMISION_CODIGO,TIPO_CAJA_CODIGO,TIPO_MOTOR_CODIGO,TIPO_AUTO_CODIGO
+from gd_esquema.Maestra m
+join gd_esquema.Fabricante f on f.FABRICANTE_NOMBRE = m.FABRICANTE_NOMBRE
+where MODELO_CODIGO is not null and TIPO_AUTO_CODIGO is not null;
+
+-- Se cargan datos de la tabla 'auto'
+insert into gd_esquema.Auto(AUTO_NRO_CHASIS,AUTO_NRO_MOTOR,AUTO_PATENTE,AUTO_FECHA_ALTA,AUTO_CANT_KMS,AUTO_MODELO)
+select distinct AUTO_NRO_CHASIS,AUTO_NRO_MOTOR,AUTO_PATENTE,AUTO_FECHA_ALTA,AUTO_CANT_KMS,MODELO_CODIGO
+from gd_esquema.Maestra
+where AUTO_NRO_CHASIS is not null;

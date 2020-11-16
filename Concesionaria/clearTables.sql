93493711c9d45203e9041c6b2634cf3486e4f357
Nahuel Pascual <npascual@est.frba.utn.edu.ar>
2020-11-15 21:03:09 -0300

    correcciones modelo BI

--- Concesionaria/clearTables.sql
+++ Concesionaria/clearTables.sql
@@ -1,3 +1,4 @@
+-- relational model
 drop table NAPOLITANA_CON_FRITAS.Item_Compra_Auto;
 drop table NAPOLITANA_CON_FRITAS.Item_Compra_Autoparte;
 drop table NAPOLITANA_CON_FRITAS.Item_Factura_Auto;
@@ -14,3 +15,23 @@ drop table NAPOLITANA_CON_FRITAS.Fabricante;
 drop table NAPOLITANA_CON_FRITAS.Cliente;
 drop table NAPOLITANA_CON_FRITAS.Sucursal;
 drop schema NAPOLITANA_CON_FRITAS;
+
+-- BI model
+-- Drop tables
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Venta_Autoparte;
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Compra_Autoparte;
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Venta_Auto;
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Compra_Auto;
+drop table NAPOLITANA_CON_FRITAS.BI_Autoparte;
+drop table NAPOLITANA_CON_FRITAS.BI_Modelo;
+drop table NAPOLITANA_CON_FRITAS.BI_Tipo_Transmision;
+drop table NAPOLITANA_CON_FRITAS.BI_Tipo_Caja;
+drop table NAPOLITANA_CON_FRITAS.BI_Tipo_Auto;
+drop table NAPOLITANA_CON_FRITAS.BI_Fabricante;
+drop table NAPOLITANA_CON_FRITAS.BI_Sucursal;
+drop table NAPOLITANA_CON_FRITAS.BI_Cantidad_Cambios;
+drop table NAPOLITANA_CON_FRITAS.BI_Tipo_Motor;
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Cliente;
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Cantidad_Rango;
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Mes;
+DROP TABLE NAPOLITANA_CON_FRITAS.BI_Potencia_Rango;
